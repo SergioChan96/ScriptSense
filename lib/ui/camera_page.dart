6fad1eca5d9d805d456454bdc4853268a9b7e652
Sergio Chan <sergio.chan@htwg-konstanz.de>
2024-06-30 17:51:43 +0200

    changed layout to fit to screen

--- lib/ui/camera_page.dart
+++ lib/ui/camera_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 
-import 'camera_provider.dart';
+import 'package:scriptsense/ui/camera_provider.dart';
 
 class CameraPage extends ConsumerWidget {
   const CameraPage({super.key});
@@ -11,7 +11,6 @@ class CameraPage extends ConsumerWidget {
   Future<void> loadImage() async {
     final picker = ImagePicker();
     XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
     if (pickedFile != null) {
 
     } else {
@@ -37,7 +36,63 @@ class CameraPage extends ConsumerWidget {
       ),
       body: cameraControllerAsyncValue.when(
         data: (cameraController) {
-          return CameraPreview(cameraController);
+          return Column(
+            children: [
+              Expanded(child: CameraPreview(cameraController)),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.1,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+
+                      },
+                      icon: Icon(Icons.flash_on)
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // Define the action to take the picture
+                      },
+                      child: LayoutBuilder(
+                        builder: (context, constraints) {
+                          double size = MediaQuery.of(context).size.height * 0.1;
+                          return Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Container(
+                                width: size,
+                                height: size,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white, // Outer ring color
+                                ),
+                              ),
+                              Container(
+                                width: size * 0.9,
+                                height: size * 0.9,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.redAccent, // Outer ring color
+                                ),
+                              ),
+                              Container(
+                                width: size * 0.85,
+                                height: size * 0.85,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white, // Inner circle color
+                                ),
+                              ),
+                            ]
+                          );
+                        }
+                      )
+                    ),
+                  ],
+                ),
+              )
+            ],
+          );
         },
         loading: () => Center(child: CircularProgressIndicator()),
         error: (err, stack) => Center(child: Text('Error: $err')),
