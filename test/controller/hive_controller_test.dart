d3e98a6e55fb22d0506a0639ef3fac684431039c
Sergio Chan <sergio.almeida@gmx.ch>
2024-07-30 20:00:12 +0200

    updated test

--- test/controller/hive_controller_test.dart
+++ test/controller/hive_controller_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
 import 'package:mockito/annotations.dart';
 import 'package:scriptsense/controller/hive_controller.dart';
 import 'package:scriptsense/model/hive_text_model.dart';
@@ -19,25 +20,34 @@ void main() {
   setUp(() {
     mockHiveBox = MockBox<HiveTextModel>();
   });
-  final HiveController controller = ProviderContainer().read(hiveControllerProvider.notifier);
-  controller.box = mockHiveBox;
-  final List<ModelWrapper> model = ProviderContainer().read(hiveControllerProvider);
+
   WidgetsFlutterBinding.ensureInitialized();
 
+  (HiveController, List<ModelWrapper>) createHiveController() {
+    final HiveController controller = ProviderContainer().read(hiveControllerProvider.notifier);
+    final List<ModelWrapper> model = ProviderContainer().read(hiveControllerProvider);
+    controller.box = mockHiveBox;
+    final KeepAliveLink keepAliveLink = controller.ref.keepAlive();
+    addTearDown(keepAliveLink.close);
+    return (controller, model);
+  }
+
   test("should initialize list", () async {
-    await controller.init();
-    expect(model.isNotEmpty, isTrue);
+    final hivePackage = createHiveController();
+    expect(hivePackage.$2.isEmpty, isTrue);
   });
 
   test("should favorite a card", () {
-    var item = model[2];
-    HiveTextModel updatedItem = HiveTextModel(
-      item.model.originalText,
-      item.model.translatedText,
-      item.model.scanDate,
-      isFavorite: !item.model.isFavorite,
+    final hivePackage = createHiveController();
+    HiveTextModel item = HiveTextModel(
+      "test",
+      "test",
+      "31/12/2023",
+      isFavorite: false,
     );
-    controller.addFavorite(2, ModelWrapper(item.index, updatedItem));
+    hivePackage.$1.box?.add(item);
+
+    controller.addFavorite(0, ModelWrapper(0, item));
     expect(model[2].model.isFavorite, updatedItem.isFavorite);
     item = model[2];
     HiveTextModel updatedItem1 = HiveTextModel(
